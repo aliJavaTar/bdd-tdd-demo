8a3e29c75080391f719a0e4417547eeb7499420b
ali <ali77erfagh99@gmail.com>
2024-12-29 10:21:57 +0330

    add project

---
+++ src/main/java/org/bro/demouom/DemoUomApplication.java
@@ -0,0 +1,13 @@
+package org.bro.demouom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bro/demouom/DemoUomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bro.demouom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
