bf4e2a44a9e3050f92fa8d17436da60ac7caff96
ali <ali77erfagh99@gmail.com>
2024-12-29 15:42:44 +0330

    refactor domain

--- src/main/java/org/bro/demouom/domain/BaseUnitOfMeasure.java
+++ src/main/java/org/bro/demouom/domain/BaseUnitOfMeasure.java
@@ -1,6 +1,5 @@
 package org.bro.demouom.domain;
 
 public record BaseUnitOfMeasure(String name, String symbol, int dimensionId) {
-    public BaseUnitOfMeasure {
-    }
+
 }
--- src/main/java/org/bro/demouom/domain/Dimension.java
+++ src/main/java/org/bro/demouom/domain/Dimension.java
@@ -1,6 +1,6 @@
 package org.bro.demouom.domain;
 
-import lombok.Getter;
+
 
 public record Dimension(int id, String name, String symbol) {
 
